4d37d4d2958a0e4283d61af252229400b4bc3819
Artur Santiago <artursantiago@info.ufrn.br>
2021-02-19 15:05:56 -0300

    dark mode

--- scripts.js
+++ scripts.js
@@ -217,3 +217,30 @@ const App = {
 }
 
 App.init();
+
+/* Dark Mode================ */
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+const switchTheme = e => {
+  if(e.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+  }
+  else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
+  }
+}
+
+toggleSwitch.addEventListener('click', switchTheme);
+
+// Verify if the user already has a preference
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
